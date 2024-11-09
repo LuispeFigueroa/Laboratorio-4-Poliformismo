fc27847fac68a6474396edba3a46a09df8309f26
fatitaprogramando <nav24044@uvg.edu.gt>
2024-11-08 20:52:49 -0600

    se agregaron las constantes del sistema

---
+++ ConstantesSistema.java
@@ -0,0 +1,22 @@
+public class ConstantesSistema {
+    // Temperatura
+    public static final float TEMP_MIN = 16.0f;
+    public static final float TEMP_MAX = 30.0f;
+        
+    // Ventilación
+    public static final String VENT_BAJA = "BAJO";
+    public static final String VENT_MEDIA = "MEDIO";
+    public static final String VENT_ALTA = "ALTO";
+        
+    // Zonas
+    public static final String ZONA_PARABRISAS = "PARABRISAS";
+    public static final String ZONA_FRONTAL = "FRONTAL";
+    public static final String ZONA_PIES = "PIES";
+        
+    // Estados
+    public static final String ESTADO_ENCENDIDO = "ENCENDIDO";
+    public static final String ESTADO_APAGADO = "APAGADO";
+    public static final String ESTADO_ERROR = "ERROR";
+    }
+    
+
